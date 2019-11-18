a687a332766e3b86de8e3b50368762aa5fdc393e
LeoRoma <xiajtn@gmail.com>
2019-11-18 16:59:49 +0000

    routing added, need to fix buttons style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MarketComponent } from './market/market.component';
 import { CollectionComponent } from './collection/collection.component';
-import { CollectableService } from "./shared/collectable.service"
+import { CollectableService } from "./shared/collectable.service";
+import { HeaderComponent } from './header.component'
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent,
     MarketComponent,
-    CollectionComponent
+    CollectionComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    routing 
   ],
   providers: [CollectableService],
   bootstrap: [AppComponent]
