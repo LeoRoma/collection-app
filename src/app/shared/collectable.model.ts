ccf6596ceb4ca183ed537518bb79f9466710838e
LeoRoma <xiajtn@gmail.com>
2019-11-18 15:53:21 +0000

    fixed bug on collectable.model

--- src/app/shared/collectable.model.ts
+++ src/app/shared/collectable.model.ts
@@ -2,7 +2,7 @@ export class Collectable {
   public description: string;
   public type: string;
 
-  contructor(description: string, type: string){
+  constructor(description: string, type: string) {
     this.description = description;
     this.type = type;
   }
